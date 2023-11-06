017440bf4b882c778ae4a237879ad8166dc0c7cc
TobyYang7 <tobyyang7@foxmail.com>
2023-11-07 00:31:27 +0800

    Removed unnecessary files and directories.

--- CSC3050_P3/alu.v
+++ CSC3050_P3/alu.v
@@ -1,81 +1,121 @@
-// flags[2] : zero flag
-// flags[1] : negative flag
-// flags[0] : overflow flag 
-
-// regA: 00000; regB: 00001
-
 module alu(
-    input[31:0] instruction, 
-    input[31:0] regA, 
-    input[31:0] regB, 
-    output reg[31:0] result, 
-    output reg[2:0] flags
+    input [31:0] instruction, 
+    input [31:0] regA, 
+    input [31:0] regB, 
+    output reg [31:0] result, 
+    output reg [2:0] flags
 );
 
-    wire[5:0] opcode;
-    reg[5:0] funct;
-    reg[4:0] rs, rt, rd, shamt;
-    reg[15:0] immediate;
-    reg[31:0] rs_reg, rt_reg;
+    wire [5:0] opcode;
+    reg [5:0] funct;
+    reg [4:0] rs, rt, rd, shamt;
+    reg [15:0] immediate;
+    reg [31:0] rs_reg, rt_reg;
 
-    reg[31:0] temp_reg;
-    integer i; // sra, srav
+    reg [31:0] temp_reg;
+    reg zero, negative, overflow; // flags
 
     assign opcode = instruction[31:26];
 
     // parse instruction
     always @(*) begin
-        // R-type
-        if (opcode == 6'b0 ) begin 
-            rs = instruction[25:21];
-            rt = instruction[20:16];
-            rd = instruction[15:11];
-            shamt = instruction[10:6];
-            funct = instruction[5:0];
-        end
-        // I-type. We only consider some R-type and I-type instructions in this simple ALU
-        else begin 
-            rs = instruction[25:21];
-            rt = instruction[20:16];
-            immediate = instruction[15:0];
+        // Extracting fields from the instruction
+        funct = instruction[5:0];
+        rs = instruction[25:21];
+        rt = instruction[20:16];
+        rd = instruction[15:11];
+        shamt = instruction[10:6];
+        immediate = instruction[15:0];
+
+        // Sign-extend immediate for I-type instructions
+        if (opcode == 6'b001000 || opcode == 6'b001100 || opcode == 6'b001101) begin
+            immediate = {{16{instruction[15]}}, immediate};
         end
     end
 
     // fetch register values
     always @(*) begin
-        if (rs == 5'b0) begin
-            rs_reg = regA;
-        end
-        else begin
-            rs_reg = regB;
-        end
-        if (rt == 5'b0) begin
-            rt_reg = regA;
-        end
-        else begin
-            rt_reg = regB;
-        end
+        // Fetching register values from regA and regB based on rs and rt
+        rs_reg = (rs == 5'b00000) ? 0 : regA;
+        rt_reg = (rt == 5'b00000) ? 0 : regB;
     end
 
-    // exicute different instructions
+    // execute different instructions
     always @(*) begin
-        flags = 3'b0;
-        // R-type
-        if (opcode == 6'b0) begin
-            case (funct)
-                // addu is implemented follows as an example, you should implement others by yourself.
-                6'h21: // addu
-                    result = rs_reg + rt_reg;
-            endcase
-        end
+        zero = 0;
+        negative = 0;
+        overflow = 0;
 
-        // I-type
-        else begin
-            case (opcode)
-                // addiu is implemented follows as an example, you should implement others by yourself.
-                6'h09: // addiu
-                    result = rs_reg + {{(16){immediate[15]}}, immediate};
-            endcase
-        end
+        // ALU operations based on opcode and funct
+        case (opcode)
+            // R-type instructions
+            6'b000000: begin
+                case (funct)
+                    // add, addu, and, or, xor, nor
+                    6'b100000, 6'b100001, 6'b100100, 6'b100101, 6'b100110, 6'b100111: begin
+                        temp_reg = rs_reg + rt_reg;
+                        overflow = (temp_reg[31] != rs_reg[31]) && (temp_reg[31] != rt_reg[31]);
+                    end
+                    // sub, subu
+                    6'b100010, 6'b100011: begin
+                        temp_reg = rs_reg - rt_reg;
+                        overflow = (temp_reg[31] != rs_reg[31]) && (temp_reg[31] != ~rt_reg[31]);
+                    end
+                    // sll, sllv, srl, srlv, sra
+                    6'b000000, 6'b000010, 6'b000011, 6'b000110, 6'b000111: begin
+                        temp_reg = (funct == 6'b000000) ? (rt_reg << shamt) :
+                                   (funct == 6'b000010) ? (rt_reg << rs_reg[4:0]) :
+                                   (funct == 6'b000011) ? (rt_reg >> shamt) : 
+                                   (funct == 6'b000110) ? (rt_reg >> rs_reg[4:0]) :
+                                   (funct == 6'b000111) ? ($signed(rt_reg) >>> shamt) : 0;
+                    end
+                    default: begin
+                        temp_reg = 0;
+                    end
+                endcase
+            end
+
+            // I-type instructions
+            6'b001000, // addi
+            6'b001100, // andi
+            6'b001101: // ori
+                begin
+                    temp_reg = rs_reg + {16'b0, immediate};
+                    overflow = (temp_reg[31] != rs_reg[31]) && (temp_reg[31] != immediate[15]);
+                end
+            6'b001110: // xori
+                begin
+                    temp_reg = rs_reg ^ {16'b0, immediate};
+                end
+            6'b000100, // beq
+            6'b000101: // bne
+                begin
+                    zero = (rs_reg == rt_reg) ? 1'b1 : 1'b0;
+                    negative = 1'b0; // Reset negative flag for branch instructions
+                end
+            6'b001010: // slti
+                begin
+                    temp_reg = (rs_reg < $signed(immediate)) ? 1 : 0;
+                    negative = temp_reg;
+                end
+            6'b001011: // sltiu
+                begin
+                    temp_reg = ($unsigned(rs_reg) < $unsigned(immediate)) ? 1 : 0;
+                end
+            6'b101011, // sw
+            6'b100011: // lw
+                begin
+                    temp_reg = rs_reg + {16'b0, immediate};
+                end
+            default: begin
+                temp_reg = 0;
+            end
+        endcase
+    end
+
+    // return result and flags
+    always @(*) begin
+        result = temp_reg;
+        flags = {zero, negative, overflow};
     end
 endmodule
--- CSC3050_P3/test_alu.v
+++ CSC3050_P3/test_alu.v
@@ -3,39 +3,58 @@
 `timescale 1ns/1ps
 
 module test_alu();
-    reg[31:0] instruction, regA, regB;
-    wire[31:0] result;
-    wire[2:0] flags;
+    reg [31:0] instruction, regA, regB;
+    wire [31:0] result;
+    wire [2:0] flags;  // Flags wire should be 3 bits to match the ALU module
 
+    // Instantiate the ALU module
     alu test(
-        .instruction (instruction),
-        .regA (regA),
-        .regB (regB),
-        .result (result), 
-        .flags (flags)
+        .instruction(instruction),
+        .regA(regA),
+        .regB(regB),
+        .result(result), 
+        .flags(flags)
     );
 
     initial begin
         $dumpfile("test.vcd");
         $dumpvars(0, test_alu);
 
-        $monitor("Time: %3d.\n Instruction: %32b\n regA: %32b, regB: %32b\n result: %32b, flags: %3b\n",
-        $time, instruction, regA, regB, result, flags);
+        $monitor("Time: %3d. Instruction: %32b\n regA: %32b, regB: %32b\n result: %32b, flags: Z=%b N=%b O=%b\n",
+        $time, instruction, regA, regB, result, flags[2], flags[1], flags[0]);
 
-        // addu
-        #1
-        $display("addu");
-        instruction = 32'b000000_00000_00001_00000_00000_100001;
-        regA = 32'b1;
-        regB = 32'b11111111_11111111_11111111_11111110; 
+        // Test cases for each instruction
 
-        // addiu
-        #1
-        $display("addiu");
-        instruction = 32'b001001_00000_00001_0111111111111111;
-        regA = 32'b01111111_11111111_11111111_11111111;
-        regB = 32'b1;
+        // ADD (R-type)
+        $display("Testing ADD...");
+        instruction = {6'b000000, 5'd0, 5'd0, 5'd0, 5'd0, 6'b100000}; // R-type format for ADD
+        regA = 32'd10;
+        regB = 32'd20;
+        #20;
 
+        // ADDI (I-type)
+        $display("Testing ADDI...");
+        instruction = {6'b001000, 5'd0, 5'd0, 16'd5}; // I-type format for ADDI
+        regA = 32'd15;
+        regB = 32'd0; // regB is not used for ADDI
+        #20;
+
+        // ADDU (R-type)
+        $display("Testing ADDU...");
+        instruction = {6'b000000, 5'd0, 5'd0, 5'd0, 5'd0, 6'b100001}; // R-type format for ADDU
+        regA = 32'd1;
+        regB = 32'hFFFFFFFE; // -2 in two's complement
+        #20;
+
+        // ADDIU (I-type)
+        $display("Testing ADDIU...");
+        instruction = {6'b001001, 5'd0, 5'd0, 16'hFFFF}; // I-type format for ADDIU
+        regA = 32'h7FFFFFFF; // Largest positive 32-bit number
+        #20;
+
+        // todo: Continue with SUB, SUBU, AND, ANDI, NOR, OR, ORI, XOR, XORI, BEQ, BNE, SLT, SLTI, SLTIU, SLTU, LW, SW, SLL, SLLV, SRL, SRLV, SRA, SRAV
+
+        // Complete the test
+        $finish;
     end
-
 endmodule
